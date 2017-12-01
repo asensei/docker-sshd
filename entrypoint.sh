5fb19e1ddf814c470f2370014069b3d8047447b4
Valerio Mazzeo <valerio.mazzeo@gmail.com>
2017-12-01 12:02:17 +0000

    fix directories permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,13 @@
 # generate host keys if not present
 ssh-keygen -A
 
+# fix permissions, if writable
+[ -w ~/.ssh/authorized_keys ] \
+    && chown root:root ~/.ssh/authorized_keys \
+    && chmod 600 ~/.ssh/authorized_keys;
+[ -w ~/.ssh ] \
+    && chown root:root ~/.ssh \
+    && chmod 700 ~/.ssh;
+
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
